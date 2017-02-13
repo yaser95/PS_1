a7b488f64da6a252adf98740a63a1015a5e18693
yaser95 <yaser@udel.edu>
2017-02-13 16:44:46 -0500

    Add files via upload

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -2,23 +2,37 @@ package pkgMain;
 
 public class Rectangle {
 
+	//attributes 
 	private double Length;
 	private double Width;
+	
+	//constructor 
 	public Rectangle(double length, double width) {
 		Length = length;
 		Width = width;
 	}
+	
+	//method to return length 
 	public double getLength() {
 		return Length;
 	}
+	
+	//method to return width 
 	public double getWidth() {
 		return Width;
 	}
 	
+	//method to calc the area 
 	public double Area()
 	{
-		// Test
 		// TODO Implement Area() function with the correct formula
-		return 0;
-	}	
+		return Length * Width;
+	}
+	//method to calc the perimeter 
+		public double Perimeter()
+		{
+			// TODO Implement Area() function with the correct formula
+			return 2 * Length + 2 * Width;
+		}		
 }
+
